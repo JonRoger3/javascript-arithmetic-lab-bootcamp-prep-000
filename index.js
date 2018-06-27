47ca27cc899f488f58906597f04f0e97dc83b04e
jonroger3@gmail.com <jonroger3@gmail.com>
2018-06-27 22:39:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,40 +1,57 @@
 var a, b;
 
+
+a = Math.floor(Math.random() * 1000)
+b = Math.floor(Math.random() * 1000)
 function add(a, b){
   return a + b;
   
 }
 
+a = Math.floor(Math.random() * 1000)
+b = Math.floor(Math.random() * 1000)
 function subtract(a, b){
   return a - b;
   
 }
 
+a = Math.floor(Math.random() * 1000)
+b = Math.floor(Math.random() * 1000)
 function multiply(a, b){
   return a * b;
   
 }
 
+a = Math.floor(Math.random() * 1000)
+b = Math.floor(Math.random() * 1000)
 function divide(a, b){
   return a / b;
   
 }
 
+a = Math.floor(Math.random() * 1000)
+b = Math.floor(Math.random() * 1000)
 function inc(a){
   return a + 1;
   
 }
 
+a = Math.floor(Math.random() * 1000)
+b = Math.floor(Math.random() * 1000)
 function dec(a){
   return a - 1;
   
 }
 
+a = Math.floor(Math.random() * 1000)
+b = Math.floor(Math.random() * 1000)
 function makeInt(n){
   return parseInt(a, 10);
   
 }
 
+a = Math.floor(Math.random() * 1000)
+b = Math.floor(Math.random() * 1000)
 function preserveDecimal(n){
   return parseFloat(a);
   
