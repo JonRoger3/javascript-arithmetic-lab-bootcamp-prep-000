31a92188905012e51cc307ed1b30aecb984b8329
jonroger3@gmail.com <jonroger3@gmail.com>
2018-06-27 22:46:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -46,7 +46,10 @@ function dec(a){
 a = Math.floor(Math.random() * 1000)
 b = Math.floor(Math.random() * 1000)
 function makeInt(n){
-  makeInt(a.toString());
+  makeInt(a.toString()){
+    return a;
+  }
+  
   
   return parseInt(a, 10);
   
